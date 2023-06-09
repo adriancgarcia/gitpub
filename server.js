064c8b8944b2d8de72495a5df8e268204ef846b1
adriancgarcia <adriancgarcia@gmail.com>
2023-06-09 08:59:54 -0500

    database setup

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-// require("dotenv").config() // loads the variables in the .env into the process.env object
 const express = require("express")
-// const morgan = require("morgan") // importing the morgan library.
-// const PORT = process.env.PORT
+
+const drinks = require("./models/drinks.js")
 const app = express()
 
-// app.use(morgan("dev")) //SETS UP OUR LOGGING MIDDLEWARE
+
+
+
 
 app.get("/greeting", (req, res) => {
     res.send("Welcome to the Gitpub App!")
 })
+
+app.get("/drinks", (req, res) => {
+    res.send(drinks)
+})
+
 
 
 
 
 
 // Listener
-
 app.listen(3000, () => {
     console.log('LISTENING ON port 3000')
 })
-// app.listen(PORT, () => {
-//     console.log(`LISTENING ON PORT ${PORT}`)
-// })
